227e737dc50fdc41d5078e70ba43eab6950257e6
Richard Craggs <rcraggs@gmail.com>
2016-02-09 10:16:41 +0000

    added more options to ash

--- a.sh
+++ a.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 directory=~/Dropbox/logs/a.sh
 
+if [ "$#" -eq 0 ]; then
+    echo "Usage"
+    echo "a.sh -p text  - apprend text to previous notebook"
+    echo "a.sh notebook text  - apprend text to notebook"
+    echo "a.sh -l  - list notebooks"
+    echo "a.sh -e notebook - edit notebook"
+    echo "a.sh -v notebook - view notebook"
+    exit
+fi
+
 if [ ! -d $directory ]
     then 
     mkdir $directory
@@ -24,7 +34,28 @@ if [ "$action" = '-v' ]
     exit 
 fi
 
+# view notebook
+if [ "$action" = '-e' ]
+    then
+    shift
+    notebook=$1
+    subl "$directory/$notebook"
+    exit 
+fi
+
+# view notebook
+if [ "$action" = '-p' ]
+    then
+    shift
+    notebook=$(<"$directory/.ash-prev")
+    text=$@
+    echo "$text" >> "$directory/$notebook"
+    exit 
+fi
+
+
 notebook=$action
 shift
 text=$@
 echo "$text" >> "$directory/$notebook"
+echo "$notebook" > "$directory/.ash-prev"
