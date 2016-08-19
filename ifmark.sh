7c1db5adc13437a75d220ebb27e3fb1e725c776f
Richard Craggs <rcraggs@gmail.com>
2016-08-19 10:29:47 +0100

    added usage note to ifmark

--- ifmark.sh
+++ ifmark.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ifmark condition file > output_file.md" 
+    exit
+fi
+
+
 file=$2
 cond=$1
 sed -e 's/<!--.*CONDITION:'$cond'[[:space:]]\(.*\)-->/\1/g' $file
